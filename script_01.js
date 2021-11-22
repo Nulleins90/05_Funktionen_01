30769e4488971380ffb158ba83c52a791f2d2b04
Christian <chris.moeller-bosse@gmx.de>
2021-11-22 12:57:32 +0100

    Unterricht

--- script_01.js
+++ script_01.js
@@ -1,2 +1,51 @@
 /***** Funktionen 01 *****/
 
+// ausgabeNamen();
+
+// function ausgabeNamen() {
+//     let firstName = "Jan";
+//     console.log("Hallo " + firstName + "!");
+// }
+
+
+// Möglichkeit 2
+// ausgabeNamenPara();
+// ausgabeNamenPara("Hans");
+// ausgabeNamenPara(prompt("Vorname?:"));
+
+// function ausgabeNamenPara(firstName) {
+//     if (firstName == "" || firstName == undefined) {
+//         firstName = "niemand";
+//     }
+//     // if (firstName == undefined) {
+//     //     firstName = "nobody";
+//     // }
+//     console.log("Hallo " + firstName + "!");
+// }
+
+// mehrere Parameter
+
+// ausgabeNamenParas("Hans", "Wurst");
+// ausgabeNamenParas(prompt("Vorname?"), prompt("Nachname?"));
+// function ausgabeNamenParas(firstName, familyName) {
+//     if (firstName == "" || firstName == undefined) {
+//         firstName = "Niemand";
+//     }
+//     if (familyName == "" || familyName == undefined) {
+//         familyName = "Niemandssohn";
+//     }
+//     console.log("Hallo " + firstName + " " + familyName + "!");
+// }
+
+// single responsibility principle
+
+function ausgabeNamenParams2(firstName, familyName) {
+
+    // 1. Funktionalität: string composing
+    const outputStr = "Hallo " + firstName + " " + familyName + "!";
+
+    // 2. Funktionalität: data output
+    console.log(outputStr);    
+}
+
+ausgabeNamenParams2("Max", "Mütze");
